6c5233ea9f813b66a0aa0f527be8376bf64184b9
Andrew <62358136+Aandrew94@users.noreply.github.com>
2020-08-28 10:34:51 +0300

    Delete hero.d

--- hero.d
+++
@@ -1,4 +0,0 @@
-obj/hero.o: /home/andrew/Documents/SimpleGame/hero.cpp \
- /home/andrew/Documents/SimpleGame/hero.h \
- /home/andrew/Documents/SimpleGame/player.h \
- /home/andrew/Documents/SimpleGame/exceptions.h
